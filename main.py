9c8cbfa2f47bd87685bda24e7ac5df6d7be6b036
ronalddtang <masamure@gmail.com>
2025-02-11 01:12:44 +1000

    added player model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -6,15 +7,19 @@ def main():
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     #infinite loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        p1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
