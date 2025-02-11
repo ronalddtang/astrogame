ea5059335e1ba1f88f3e055fc6a7d7271b10921d
ronalddtang <masamure@gmail.com>
2025-02-11 23:26:48 +1000

    added asteroids

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
 import pygame
 from player import *
 from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+#initialising groups
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+
+#init containers for the groups
+Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable,)
+
 
 def main():
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+    #initalising pygame
     pygame.init()
+
+    #setting game variables
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #creating players
     p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
+    #creating asteroids
+    rock = AsteroidField()
+  
+
+
+
     #infinite loop
     while True:
+        #makes code stop when window is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #makes the screen fill with a black bg
         screen.fill((0,0,0))
-        p1.draw(screen)
-        p1.update(dt)
+        
+        updatable.update(dt)
+        for items in drawable:
+            items.draw(screen)
+        # drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
